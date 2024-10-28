62058e0fc349f6c8eba5722f4a957078d4ba5df9
AleksBartov <37273772+AleksBartov@users.noreply.github.com>
2024-10-28 15:49:55 +0300

    data constants work

---
+++ orthodox-calendar/constants/data.ts
@@ -0,0 +1,28 @@
+export const daysAndMonthsAndYears = [
+  {
+    year: 2024,
+    months: [
+      {
+        jan: [
+          { dayName: "пн", dayNum: 1 },
+          { dayName: "вт", dayNum: 2 },
+          { dayName: "ср", dayNum: 3 },
+        ],
+      },
+      {
+        feb: [
+          { dayName: "чт", dayNum: 1 },
+          { dayName: "пт", dayNum: 2 },
+          { dayName: "сб", dayNum: 3 },
+        ],
+      },
+      {
+        mar: [
+          { dayName: "пт", dayNum: 1 },
+          { dayName: "сб", dayNum: 2 },
+          { dayName: "вс", dayNum: 3 },
+        ],
+      },
+    ],
+  },
+];
